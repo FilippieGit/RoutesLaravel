28483d5f71dffca9fb47b51ec4d232899402d735
FilippieGit <filippieamaral@gmail.com>
2025-03-31 22:15:19 -0300

    prefix/api

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[\App\Http\Controllers\principalController::class,'principal'])->name('site.principal');
 Route::get('/contato',[\App\Http\Controllers\contatoController::class,'contato'])->name('site.contato');
 Route::get('/sobre-nos',[\App\Http\Controllers\sobrenosController::class,'sobreNos'])->name('site.sobre-nos');
+
+Route::prefix("/api")->group(function(){
+    
+    Route::get('/produtos', function(){
+        return 'Produtos';
+    });
+    Route::get('/fornecedores', function(){
+        return 'Fornecedores';
+    });
+    Route::get('/clientes', function(){
+        return 'Clientes';
+    });
+
+});
+
+Route::get('/login', function(){
+    return 'Login';
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe.
+    <a href="'.route('site.principal').'">Clique aqui</a>
+    para ir para página inicial';
+});
